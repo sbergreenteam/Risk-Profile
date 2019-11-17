b5d4e816b6ec350134395b9daa17fd37a3205cb4
jemzza <42701230+jemzza@users.noreply.github.com>
2019-11-17 10:14:06 +0300

    Add files via upload

--- Risk Profile/Controller/RiskProfileViewController.swift
+++ Risk Profile/Controller/RiskProfileViewController.swift
@@ -11,8 +11,20 @@ import UIKit
 class RiskProfileViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     var questions = [Question]()
+
+        let previousButton: UIButton = {
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            button.layer.cornerRadius = 15
+            button.setTitle("ПРЕД", for: .normal)
+            button.setTitleColor(.white, for: .normal)
+//            button.backgroundColor = #colorLiteral(red: 0.231372549, green: 0.6705882353, blue: 0.6823529412, alpha: 1)
+            button.backgroundColor = #colorLiteral(red: 0.1568627451, green: 0.6156862745, blue: 0.3882352941, alpha: 1)
+            button.addTarget(self, action: #selector(buttonTouchDownNormalPrev), for: .touchCancel)
+            button.addTarget(self, action: #selector(handlePrev), for: .touchCancel)
+            return button
+        }()
     
-    private let previousButton = UIButton(title: "ПРЕД", titleColor: .gray, font: .boldSystemFont(ofSize: 14), target: self, action: #selector(handlePrev))
+//    private let previousButton = UIButton(title: "ПРЕД", titleColor: .gray, font: .boldSystemFont(ofSize: 14), target: self, action: #selector(handlePrev))
     
     @objc private func handlePrev() {
         let nextIndex = max(pageControl.currentPage - 1, 0)
@@ -21,7 +33,19 @@ class RiskProfileViewController: UICollectionViewController, UICollectionViewDel
         collectionView?.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
     }
     
-    private let nextButton = UIButton(title: "СЛЕД", titleColor: .rgb(red: 78, green: 228, blue: 78), font: .boldSystemFont(ofSize: 14), target: self, action: #selector(handleNext))
+    let nextButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        button.layer.cornerRadius = 15
+        button.setTitle("СЛЕД", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+//        button.backgroundColor = #colorLiteral(red: 0.231372549, green: 0.6705882353, blue: 0.6823529412, alpha: 1)
+        button.backgroundColor = #colorLiteral(red: 0.1568627451, green: 0.6156862745, blue: 0.3882352941, alpha: 1)
+        button.addTarget(self, action: #selector(buttonTouchDownNormalNext), for: .touchCancel)
+        button.addTarget(self, action: #selector(handleNext), for: .touchCancel)
+        return button
+    }()
+    
+//    private let nextButton = UIButton(title: "СЛЕД", titleColor: .rgb(red: 78, green: 228, blue: 78), font: .boldSystemFont(ofSize: 14), target: self, action: #selector(handleNext))
     
     @objc private func handleNext() {
         let nextIndex = min(pageControl.currentPage + 1, questions.count - 1)
@@ -29,12 +53,13 @@ class RiskProfileViewController: UICollectionViewController, UICollectionViewDel
         pageControl.currentPage = nextIndex
         collectionView?.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
     }
-    
+
     lazy var pageControl: UIPageControl = {
         let pc = UIPageControl()
         pc.currentPage = 0
         pc.numberOfPages = self.questions.count
-        pc.currentPageIndicatorTintColor = UIColor.rgb(red: 78, green: 228, blue: 78)
+//        pc.currentPageIndicatorTintColor = UIColor.rgb(red: 78, green: 228, blue: 78)
+        pc.currentPageIndicatorTintColor =  #colorLiteral(red: 0.1568627451, green: 0.6156862745, blue: 0.3882352941, alpha: 1)
         pc.pageIndicatorTintColor = UIColor.rgb(red: 249, green: 207, blue: 224)
         return pc
     }()
@@ -75,17 +100,44 @@ class RiskProfileViewController: UICollectionViewController, UICollectionViewDel
         collectionView?.isPagingEnabled = true
         
         
+        let tapGestureRecognizerForPrev = UITapGestureRecognizer(target: self, action: #selector(addPulse(sender:)))
+        tapGestureRecognizerForPrev.numberOfTapsRequired = 1
+        previousButton.addGestureRecognizer(tapGestureRecognizerForPrev)
         
+        let tapGestureRecognizerForNext = UITapGestureRecognizer(target: self, action: #selector(addPulse(sender:)))
+        tapGestureRecognizerForNext.numberOfTapsRequired = 1
+        nextButton.addGestureRecognizer(tapGestureRecognizerForNext)
+        
+        nextButton.pulsate()
+        previousButton.pulsate()
     }
     
     private func downloadQuestions() {
         self.questions = Bundle.main.decode([Question].self, from: "questions.json")
-        
     }
     
     private func registerCells() {
         collectionView?.register(PageCell.self, forCellWithReuseIdentifier: "cellId")
         collectionView.register(FinalCell.self, forCellWithReuseIdentifier: "finalCellId")
+    }
+    
+     @objc
+        func addPulse(sender: UITapGestureRecognizer){
+            let position = sender.location(in: view)
+            let pulse = Pulsing(numberOfPulses: 1, radius: 100, position: position)
+            pulse.animationDuration = 0.5
+            pulse.backgroundColor = #colorLiteral(red: 0.5333333333, green: 0.2666666667, blue: 0.5803921569, alpha: 1)
+            self.view.layer.insertSublayer(pulse, below: nextButton.layer)
+        }
+    
+    @objc
+    func buttonTouchDownNormalNext() {
+        nextButton.hiddenBtn()
+    }
+    
+    @objc
+    func buttonTouchDownNormalPrev() {
+        previousButton.hiddenBtn()
     }
 
 }
@@ -144,6 +196,64 @@ extension RiskProfileViewController {
     
 }
 
+extension UIButton {
+    func pulsate() {
+    let pulse = CASpringAnimation(keyPath: "transform.scale")
+            pulse.duration = 0.6
+
+            pulse.fromValue = 0.80
+        pulse.toValue = 0.85
+            pulse.autoreverses = true
+            pulse.repeatCount = HUGE
+            pulse.initialVelocity = 0.5
+            pulse.damping = 1.0
+            pulse.speed = 0.6
+            layer.add(pulse, forKey: "pulse")
+        }
+    
+    func flash() {
+    let flash = CABasicAnimation(keyPath: "opacity")
+            flash.duration = 0.5
+            flash.fromValue = 1
+            flash.toValue = 0.7
+        flash.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+            flash.autoreverses = true
+            flash.repeatCount = HUGE
+            layer.add(flash, forKey: nil)
+        }
+    
+    func shake() {
+        let shake = CABasicAnimation(keyPath: "position")
+            shake.duration = 0.1
+            shake.repeatCount = 2
+            shake.autoreverses = true
+        let fromPoint = CGPoint(x: center.x - 10, y: center.y)
+        let fromValue = NSValue(cgPoint: fromPoint)
+        let toPoint = CGPoint(x: center.x + 8, y: center.y)
+        let toValue = NSValue(cgPoint: toPoint)
+        let fromPoint2 = CGPoint(x: center.x - 0, y: center.y)
+        let fromValue2 = NSValue(cgPoint: fromPoint2)
+        let toPoint2 = CGPoint(x: center.x + 15, y: center.y)
+        let toValue2 = NSValue(cgPoint: toPoint2)
+            shake.fromValue = fromValue
+            shake.toValue = toValue
+            shake.fromValue = fromValue2
+            shake.toValue = toValue2
+            layer.add(shake, forKey: "position")
+        }
+    
+    func hiddenBtn() {
+    let pulse = CASpringAnimation(keyPath: "transform.scale")
+        let flash = CABasicAnimation(keyPath: "opacity")
+        flash.duration = 0.7
+            flash.fromValue = 1
+            flash.toValue = 0
+        flash.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+            flash.autoreverses = true
+            flash.repeatCount = 1
+            layer.add(flash, forKey: nil)
+        }
+}
 
 
 
--- Risk Profile/View/PageCell.swift
+++ Risk Profile/View/PageCell.swift
@@ -10,19 +10,34 @@ import UIKit
 
 class PageCell: UICollectionViewCell {
     
+    
     var page: Question? {
         didSet {
             guard let unwrappedPage = page else { return }
-            
+            guard let unwrappedAnswer = page?.answers else {return}
             localImageView.image = UIImage(named: unwrappedPage.imageName)
             
-            let attributedText = NSMutableAttributedString(string: unwrappedPage.question, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
-            
+            let attributedText = NSMutableAttributedString(string: unwrappedPage.question, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 10)])
+            let buttonAText = NSMutableAttributedString(string: unwrappedAnswer.a, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+            let buttonBText = NSMutableAttributedString(string: unwrappedAnswer.b, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+            let buttonCText = NSMutableAttributedString(string: unwrappedAnswer.c ?? "", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+            let buttonDText = NSMutableAttributedString(string: unwrappedAnswer.d ?? "", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+            let buttonEText = NSMutableAttributedString(string: unwrappedAnswer.e ?? "", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
             descriptionTextView.attributedText = attributedText
             descriptionTextView.textAlignment = .center
+            button1.titleLabel?.attributedText = buttonAText
+            button2.titleLabel?.attributedText = buttonBText
+            button3.titleLabel?.attributedText = buttonCText
+            button4.titleLabel?.attributedText = buttonDText
         }
     }
     
+    
+    let button1 = UIButton()
+      let button2 = UIButton()
+      let button3 = UIButton()
+      let button4 = UIButton()
+      let button5 = UIButton()
     
     
     private let localImageView: UIImageView = {
@@ -50,29 +65,82 @@ class PageCell: UICollectionViewCell {
     }
     
     private func setupLayout() {
-        let topImageContainerView = UIView()
-        addSubview(topImageContainerView)
         
-        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        button1.titleLabel?.numberOfLines = 0
+        button2.titleLabel?.numberOfLines = 0
+        button3.titleLabel?.numberOfLines = 0
+        button4.titleLabel?.numberOfLines = 0
+        button5.titleLabel?.numberOfLines = 0
+        
+        button1.titleLabel?.text = page?.answers.a
+        button2.titleLabel?.text = page?.answers.b
+        button3.titleLabel?.text = page?.answers.c
+        button4.titleLabel?.text = page?.answers.d
+        
+//        button1.titleLabel?.adjustsFontSizeToFitWidth = true
+//        button2.titleLabel?.adjustsFontSizeToFitWidth = true
+//        button3.titleLabel?.adjustsFontSizeToFitWidth = true
+//        button4.titleLabel?.adjustsFontSizeToFitWidth = true
+//        button5.titleLabel?.adjustsFontSizeToFitWidth = true
+   
+        
+        let topContainerView = UIView()
+            addSubview(topContainerView)
+            topContainerView.translatesAutoresizingMaskIntoConstraints = false
 
-        topImageContainerView.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
-        topImageContainerView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        topImageContainerView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+            topContainerView.topAnchor.constraint(equalTo: topAnchor).isActive = true
 
-        topImageContainerView.addSubview(localImageView)
-        
-        localImageView.centerXAnchor.constraint(equalTo: topImageContainerView.safeAreaLayoutGuide.centerXAnchor).isActive = true
-        localImageView.centerYAnchor.constraint(equalTo: topImageContainerView.safeAreaLayoutGuide.centerYAnchor).isActive = true
-        localImageView.heightAnchor.constraint(equalTo: topImageContainerView.safeAreaLayoutGuide.heightAnchor).isActive = true
+            topContainerView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+            topContainerView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
 
-        topImageContainerView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3).isActive = true
-        
-        addSubview(descriptionTextView)
-        
-        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
-        descriptionTextView.leftAnchor.constraint(equalTo: leftAnchor, constant: 24).isActive = true
-        descriptionTextView.rightAnchor.constraint(equalTo: rightAnchor, constant: -24).isActive = true
-        descriptionTextView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0).isActive = true
+            topContainerView.addSubview(localImageView)
+            localImageView.topAnchor.constraint(equalTo: topContainerView.topAnchor, constant: 100).isActive = true
+            localImageView.centerXAnchor.constraint(equalTo: topContainerView.centerXAnchor).isActive = true
+            localImageView.heightAnchor.constraint(equalTo: topContainerView.heightAnchor, multiplier: 0.2).isActive = true
+           
+        topContainerView.addSubview(descriptionTextView)
+            descriptionTextView.topAnchor.constraint(equalTo: localImageView.bottomAnchor).isActive = true
+            descriptionTextView.leftAnchor.constraint(equalTo: topContainerView.leftAnchor, constant: 0).isActive = true
+            descriptionTextView.rightAnchor.constraint(equalTo: topContainerView.rightAnchor, constant: 0).isActive = true
+            topContainerView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.9).isActive = true
+            
+            let radioButtonsContainerView = UIView()
+            radioButtonsContainerView.translatesAutoresizingMaskIntoConstraints = false
+            topContainerView.addSubview(radioButtonsContainerView)
+            
+            radioButtonsContainerView.topAnchor.constraint(equalTo: descriptionTextView.bottomAnchor).isActive = true
+            radioButtonsContainerView.bottomAnchor.constraint(equalTo: topContainerView.bottomAnchor).isActive = true
+            radioButtonsContainerView.leftAnchor.constraint(equalTo: topContainerView.leftAnchor, constant: 0).isActive = true
+            radioButtonsContainerView.rightAnchor.constraint(equalTo: topContainerView.rightAnchor, constant: 0).isActive = true
+            radioButtonsContainerView.heightAnchor.constraint(equalTo: descriptionTextView.heightAnchor, multiplier: 0.5).isActive = true
+            [button1, button2, button3, button4, button5].forEach { (button) in
+                button.backgroundColor = .green
+                button.translatesAutoresizingMaskIntoConstraints = false
+            }
+            [button1, button2, button3, button4, button5].forEach { radioButtonsContainerView.addSubview($0)
+            }
+            
+            button2.widthAnchor.constraint(equalTo: button1.widthAnchor).isActive = true
+            button2.heightAnchor.constraint(equalTo: button1.heightAnchor).isActive = true
+            button3.widthAnchor.constraint(equalTo: button1.widthAnchor).isActive = true
+            button3.heightAnchor.constraint(equalTo: button1.heightAnchor).isActive = true
+            button4.widthAnchor.constraint(equalTo: button1.widthAnchor).isActive = true
+            button4.heightAnchor.constraint(equalTo: button1.heightAnchor).isActive = true
+            button5.widthAnchor.constraint(equalTo: button1.widthAnchor).isActive = true
+            button5.heightAnchor.constraint(equalTo: button1.heightAnchor).isActive = true
+            button1.leadingAnchor.constraint(equalTo: radioButtonsContainerView.leadingAnchor).isActive = true
+            button1.trailingAnchor.constraint(equalTo: radioButtonsContainerView.trailingAnchor).isActive = true
+            button2.centerYAnchor.constraint(equalTo: button1.centerYAnchor).isActive = true
+            button3.centerYAnchor.constraint(equalTo: button1.centerYAnchor).isActive = true
+            button4.centerYAnchor.constraint(equalTo: button1.centerYAnchor).isActive = true
+            button5.centerYAnchor.constraint(equalTo: button1.centerYAnchor).isActive = true
+            
+            button1.topAnchor.constraint(equalTo: radioButtonsContainerView.topAnchor, constant: 2).isActive = true
+            button1.bottomAnchor.constraint(equalTo: button2.topAnchor, constant: 2).isActive = true
+            button2.bottomAnchor.constraint(equalTo: button3.topAnchor, constant: 2).isActive = true
+            button3.bottomAnchor.constraint(equalTo: button4.topAnchor, constant: 2).isActive = true
+            button4.bottomAnchor.constraint(equalTo: button5.topAnchor, constant: 2).isActive = true
+            button5.bottomAnchor.constraint(equalTo: radioButtonsContainerView.bottomAnchor, constant: -2).isActive = true
     
     }
     
